376ba9b47128023bbf84f1694f6662935c1834f1
Emir Can Tokmak <2014210213008@ogrenci.karabuk.edu.tr>
2022-09-07 08:18:13 +0300

    v1.011

---
+++ 1IlkProgramveVeriTipleri/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿// İlk program Hello World
+
+using System;
+
+class ilkProgram
+{
+    public static void Main() {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ 1IlkProgramveVeriTipleri/veriTipleri/veriTipleri/Program.cs
@@ -0,0 +1,35 @@
+﻿//Veri tipleri çalışması
+
+using System;
+
+class temelVeriTipleri
+{
+    public static void Main()
+    {
+        //C# Veri Tipleri Çalışmaları
+        /*
+         C# Temel veri tipleri şunlardır;
+        *byte
+        *sbyte
+        *short
+        *ushort
+        *int
+        *long
+        *ulong
+        *double
+        *float
+        */
+        //
+        int a;
+        int b = 10;
+        short c;
+        short d = 22;
+        double boy = 1.68;
+        float pi;
+   
+
+
+        Console.WriteLine("Tam Sayı={0} double veri {1}",b,boy);
+
+    }
+}
